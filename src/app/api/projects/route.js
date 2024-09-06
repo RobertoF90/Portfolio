24544d938ac9b7b368f7debe9eb7ef2e562630b0
Roberto Franco <robertofranco8090@gmail.com>
2024-09-06 17:30:32 +0200

    fixing bug

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -1,5 +1,7 @@
 import Project from '@/models/project';
 import connectDB from '@/lib/utils/database';
+
+export const dynamic = 'force-dynamic';
 export async function GET() {
   try {
     await connectDB();
